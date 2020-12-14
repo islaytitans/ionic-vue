40bd28b872e286624cd8458ded0f1ede4c52a7f5
Jonathan Robbins <jonathan.m.robbins@gmail.com>
2020-12-14 16:23:19 +0000

    intial creation of the form

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,13 +1,44 @@
 <template>
   <base-layout page-title="Add a memory" page-default-back-link="/memories">
-    <h2>Add a memory</h2>
+    <form class="ion-padding">
+      <ion-list>
+        <ion-item>
+          <ion-label position="floating">Title</ion-label>
+          <ion-input type="text" required />
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Description</ion-label>
+          <ion-textarea type="text" rows="5" required />
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Image Url</ion-label>
+          <ion-input type="url" required />
+        </ion-item>
+      </ion-list>
+      <ion-button expand="full">Save</ion-button>
+    </form>
   </base-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonButton,
+} from "@ionic/vue";
 
 export default defineComponent({
-
+  components: {
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonButton,
+  },
 });
 </script>
