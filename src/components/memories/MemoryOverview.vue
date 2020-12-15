9ac6247e9c2cf4886d0786ee7ed2525ca16203ca
Jonathan Robbins <jonathan.m.robbins@gmail.com>
2020-12-15 15:53:22 +0000

    implemented code to merge image upon photo taken on PWA

--- src/components/memories/MemoryOverview.vue
+++ src/components/memories/MemoryOverview.vue
@@ -1,5 +1,6 @@
 <template>
-  <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+  <!-- <ion-img :src="memory.image" :alt="memory.title"></ion-img> -->
+  <ion-img :src="editedImage" :alt="memory.title" crossorigin="anonymous"></ion-img>
   <h2 class="ion-text-center">{{ memory.title }}</h2>
   <p class="ion-text-center">{{ memory.description }}</p>
 </template>
@@ -7,11 +8,31 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { IonImg } from "@ionic/vue";
+import mergeImages from "merge-images";
 
 export default defineComponent({
   props: ["memory"],
+  data() {
+    return {
+      editedImage: ''
+    }
+  },
   components: {
     IonImg,
   },
+  methods: {
+    mergeImage(): void {
+      mergeImages([
+        { src: this.memory.image, x: 0, y:0 },
+        { src: '../../img/body.png', x: 200, y:0 },
+        { src: '../../img/eyes.png', x: 200, y:0 },
+        { src: '../../img/mouth.png', x: 200, y:0 }])
+        .then(b64 => this.editedImage = b64)
+        console.log(this.editedImage);
+    }
+  },
+  created() {
+    this.mergeImage();
+  }
 });
 </script>
