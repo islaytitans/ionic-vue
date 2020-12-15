1cadebbe8bcc52f3f18ecb7f3cc1f512aa17ad0b
Jonathan Robbins <jonathan.m.robbins@gmail.com>
2020-12-15 11:43:27 +0000

    initial work on camera

--- src/components/memories/CreateMemoryForm.vue
+++ src/components/memories/CreateMemoryForm.vue
@@ -10,8 +10,13 @@
         <ion-textarea type="text" rows="5" required v-model="enteredDescription" />
       </ion-item>
       <ion-item>
-        <ion-label position="floating">Image Url</ion-label>
-        <ion-input type="url" required v-model="enteredImage"/>
+        <ion-thumbnail slot="start">
+          <img src="" alt="" />
+        </ion-thumbnail>
+        <ion-button type="button" fill="clear" @click="takePhoto">
+          <ion-icon slot="start" :icon="camera"></ion-icon>
+          Take a photo
+        </ion-button>
       </ion-item>
     </ion-list>
     <ion-button type="submit" expand="full">Save</ion-button>
@@ -27,8 +32,14 @@ import {
   IonInput,
   IonTextarea,
   IonButton,
+  IonThumbnail,
+  IonIcon
 } from "@ionic/vue";
+import { CameraPhoto, CameraResultType, CameraSource, Plugins } from '@capacitor/core';
+import { camera } from 'ionicons/icons';
 import Memory from "@/interfaces/Memory";
+
+const { Camera } = Plugins;
 
 export default defineComponent({
   emits: ['save-memory'],
@@ -39,12 +50,16 @@ export default defineComponent({
     IonInput,
     IonTextarea,
     IonButton,
+    IonThumbnail,
+    IonIcon
   },
   data() {
     return {
       enteredTitle: '',
       enteredDescription: '',
-      enteredImage: ''
+      enteredImage: '',
+      previewImageUrl: '',
+      camera
     }
   },
   methods: {
@@ -55,6 +70,17 @@ export default defineComponent({
         image: this.enteredImage,
       };
       this.$emit('save-memory', memoryData);
+    },
+    async takePhoto() {
+      const photo: CameraPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Prompt,
+        quality: 60
+      });
+
+      if (photo.webPath) {
+        this.previewImageUrl = photo.webPath.toString();
+      }
     }
   }
 });
